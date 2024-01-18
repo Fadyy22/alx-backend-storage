95569400db71e72664e7380baafc66cdc8a5f658
Fadyy22 <fadyalaa441@gmail.com>
2024-01-18 15:00:57 +0200

    create view for students who need meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@ DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score < 80 AND last_meeting IS NULL OR last_meeting > 1;
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting > 1);
