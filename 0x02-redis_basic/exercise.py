de168e816ad3556a814c34bdde10ab444edd7163
Fadyy22 <fadyalaa441@gmail.com>
2024-01-25 00:25:09 +0200

    add get get_str get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module for redis tasks in python"""
 
-from typing import Union
+from typing import Callable, Union
 from uuid import uuid4
 import redis
 
@@ -15,7 +15,22 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """stores data passed in redis client and return the key"""
+        """stores data passed in redis data storage and return the key"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """returns a value from redis data storage by using key"""
+        if fn:
+            return fn(self._redis.get(key))
+        else:
+            return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """returns str value for redis data storage"""
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        """returns str value for redis data storage"""
+        return self.get(key, fn=int)
