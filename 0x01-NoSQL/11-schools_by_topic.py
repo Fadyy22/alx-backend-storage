bb9d3aa109af642f02320dd1a576c73398eb437e
Fadyy22 <fadyalaa441@gmail.com>
2024-01-27 01:31:09 +0200

    get all documents which have certain topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""module containing schools_by_topic function"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function that returns the list of school having a specific topic"""
+    return mongo_collection.find({"topics": {"$elemMatch": topic}})
