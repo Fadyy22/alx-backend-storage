7cbb175afd704be45414be95a1b640e3f90125f9
Fadyy22 <fadyalaa441@gmail.com>
2024-01-27 01:07:57 +0200

    insert document in collection

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,4 +5,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """function that inserts a new document in a collection based on kwargs"""
-    return mongo_collection.insert_one(kwargs)
+    document = mongo_collection.insert_one(kwargs)
+    return document.inserted_id
